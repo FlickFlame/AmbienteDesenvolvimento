68327c900a23e40dbb839f93546593158f787b20
João Victor <CotimMou@gmail.com>
2019-03-18 10:35:17 -0300

    Colocando assertions para proteger o código

--- Assets/Scripts/SceneManage.cs
+++ Assets/Scripts/SceneManage.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class SceneManage : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class SceneManage : MonoBehaviour
     private Vector3 TapeSpeed = new Vector3(-2f,0f,0f);
     [SerializeField]
     private Transform Tape = null;
+
+    void Awake(){
+        Assert.IsNotNull(Tape);
+    }
 
     // Start is called before the first frame update
     void Start()

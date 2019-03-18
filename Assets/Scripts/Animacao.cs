ebd8b1c9a836ae15ca87b048b4450c5022f3b27f
João Victor <CotimMou@gmail.com>
2019-03-18 09:32:54 -0300

    Colocando limites

--- Assets/Scripts/Animacao.cs
+++ Assets/Scripts/Animacao.cs
@@ -6,6 +6,14 @@ public class Animacao : MonoBehaviour
 {
     [SerializeField]
     private float MoveSpeed = 1f;
+    [SerializeField]
+    private float LimitTop = 4f;
+    [SerializeField]
+    private float LimitBottom = -4f;
+    [SerializeField]
+    private float LimitLeft = -8f;
+    [SerializeField]
+    private float LimitRight = 8f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +28,21 @@ public class Animacao : MonoBehaviour
         Vector3 velocity = new Vector3(HorizontalInput,VerticalInput,0f) * MoveSpeed;
 
         transform.position = transform.position + velocity * Time.deltaTime;
+
+        if(transform.position.y < LimitBottom){
+            transform.position = new Vector3(transform.position.x,LimitBottom,transform.position.z);
+        }
+
+        if(transform.position.y > LimitTop){
+            transform.position = new Vector3(transform.position.x,LimitTop,transform.position.z);
+        }
+
+        if(transform.position.x < LimitLeft){
+            transform.position = new Vector3(LimitLeft,transform.position.y,transform.position.z);
+        }
+
+        if(transform.position.x > LimitRight){
+            transform.position = new Vector3(LimitRight,transform.position.y,transform.position.z);
+        }
     }
 }

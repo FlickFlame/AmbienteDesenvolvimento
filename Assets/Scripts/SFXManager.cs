d50dc3ed9322441244caff677cbf9fbfb57ca704
João <CotimMou@gmail.com>
2019-03-25 11:02:42 -0300

    Pegando Moeda Sem Soltar Particles

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager instance;
+
+    public GameObject CoinParticle;
+    void Awake(){
+        if(instance == null){
+            instance = this;
+        }
+    }
+    public void ShowCoinParticle(GameObject obj){
+        Instantiate(CoinParticle, obj.transform.position,Quaternion.identity);
+    }
+}

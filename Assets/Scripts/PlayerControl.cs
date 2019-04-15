e82acf503db7d9175d86c2dc5360412b443787e9
João <CotimMou@gmail.com>
2019-04-15 09:45:05 -0300

    Contando moedas

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -18,6 +18,7 @@ public class PlayerControl : MonoBehaviour
             SFXManager.instance.ShowCoinParticle(other.gameObject);
             AudioManager.instance.PlaySoundCoinPickup(other.gameObject);
             Destroy(other.gameObject);
+            SceneManager.instance.IncrementCoinCount();
         }
     }
 }
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -3,27 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class SceneManage : MonoBehaviour
+public class SceneManager : MonoBehaviour
 {
+    public static SceneManager instance = null;
     [SerializeField]
     private Vector3 TapeSpeed = new Vector3(-2f,0f,0f);
     [SerializeField]
     private Transform Tape = null;
     public UIComponents uiComponents;
+    SceneData sceneData = new SceneData();
 
     void Awake(){
         Assert.IsNotNull(Tape);
+        if(instance ==null){
+            instance = this;
+        }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         Tape.position = Tape.position + TapeSpeed * Time.deltaTime;
+        DisplayHudData();
+    }
+
+public void IncrementCoinCount(){
+    sceneData.coinCount++;
+}
+    void DisplayHudData(){
+        uiComponents.hud.txtCoinCount.text = "x " + sceneData.coinCount;
     }
 }
